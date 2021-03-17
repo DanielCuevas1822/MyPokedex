ee16526433ece8120a6eb8d78eedd7d25971dbc9
DanielCuevas1822 <daniel_cuevas_hernandez@outlook.com>
2021-03-17 13:57:29 -0600

    Create a Button and SearchBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../assets/styles/components/SearchBar.scss";
+
+const SearchBar = ({ handleSubmit, handleChange, searching }) => {
+  return (
+    <div className="col-sm-12 poke-search d-flex justify-content-center">
+      <form onSubmit={handleSubmit} className="row g-3">
+        <div className="col-9">
+          <input
+            className="search"
+            type="text"
+            placeholder="Search a Pokémon"
+            onChange={handleChange}
+            value={searching}
+          />
+        </div>
+
+        <div className="col-3">
+          <button type="submit" className="btn poke__btn mb-3">
+            <i className="bi bi-search"></i>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
